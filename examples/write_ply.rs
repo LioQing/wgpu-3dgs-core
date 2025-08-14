cb0ef18f03993b25087ef0f67d87f316f9783e56
LioQing <s2010157@gmail.com>
2025-08-14 20:38:54 +0800

    Add write_ply example

---
+++ examples/write_ply.rs
@@ -0,0 +1,35 @@
+//! This example generates a PLY file containing 3 hardcoded Gaussians.
+
+use glam::*;
+use wgpu_3dgs_core as gs;
+
+fn main() {
+    let gaussians = gs::Gaussians {
+        gaussians: vec![
+            gs::Gaussian {
+                rot: Quat::from_axis_angle((Vec3::X + Vec3::Y / 2.0 + Vec3::Z).normalize(), 0.5),
+                pos: Vec3::ZERO,
+                scale: Vec3::new(0.5, 1.0, 0.75),
+                color: U8Vec4::new(255, 0, 0, 255),
+                sh: [Vec3::ZERO; 15],
+            },
+            gs::Gaussian {
+                rot: Quat::from_axis_angle((Vec3::X + Vec3::Z / 3.0).normalize(), 0.3),
+                pos: Vec3::new(0.0, 8.0, 4.0),
+                scale: Vec3::new(1.0, 1.9, 0.75),
+                color: U8Vec4::new(0, 255, 0, 255),
+                sh: [Vec3::ZERO; 15],
+            },
+            gs::Gaussian {
+                rot: Quat::from_axis_angle((Vec3::X - Vec3::Z).normalize(), 0.2),
+                pos: Vec3::new(4.0, 0.0, 6.0),
+                scale: Vec3::new(1.0, 1.1, 0.8),
+                color: U8Vec4::new(0, 0, 255, 255),
+                sh: [Vec3::ZERO; 15],
+            },
+        ],
+    };
+
+    let mut file = std::fs::File::create("target/out.ply").expect("created file");
+    gaussians.write_ply(&mut file).expect("write PLY file");
+}
